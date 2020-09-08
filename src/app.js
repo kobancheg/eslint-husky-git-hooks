4b609642051b50cb733864bd6c22db67d7cf3efe
Aleksey Kozhukhov <aleks.kozhukhov@MacBook-Air.local>
2020-09-08 14:24:53 +0300

    rules, environment, redactor settings

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
+/* eslint-disable no-console */
 const a = 1;
 
 function foo(name) {
-  const lastName = name;
-  return lastName;
+	const lastName = name;
+	name = 1;
+	return lastName;
 }
+let x = 200;
 
-const x = 200;
+x++;
 
 foo('Denis');
+console.log(window);
 
+// eslint-disable-next-line no-console
 console.log(x, a);
+
+// qwrqewitjqwoijqwdopijqwdqowdjgq[owdgjqow[gjqowgjqowgjqowidjgqodijwtuetuwretiyrqiughsdjfhgasdjhgajsdgfajhskdgkajghsskajgakhj]]
+
+const arrow = (y) => y + 1;
+arrow(2);
+
+const obj = {
+	name: 'Alex',
+};
+
+const { name } = obj;
